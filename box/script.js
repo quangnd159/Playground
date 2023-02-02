0d7a0fc20ef51dfc8783489dc2d2b70813c68273
Quang <quangnd159@gmail.com>
2023-02-02 10:54:20 +0700

    style mystery box

--- box/script.js
+++ box/script.js
@@ -5,10 +5,20 @@ const resetButton = document.getElementById("reset-button");
 const items = [-500, -400, -300, -200, -100, 100, 200, 300, 400, 500, 1000, "💩", "🔄"];
 
 mysteryBox.addEventListener("click", function () {
+  mysteryBox.classList.remove("unopened");
+  mysteryBox.classList.add("opened");
+  questionMark.classList.remove("unopened");
+  questionMark.classList.add("opened");
+
   const randomIndex = Math.floor(Math.random() * items.length);
   questionMark.innerHTML = items[randomIndex];
 });
 
+
 resetButton.addEventListener("click", function () {
   questionMark.innerHTML = "?";
+  mysteryBox.classList.remove("opened");
+  mysteryBox.classList.add("unopened");
+  questionMark.classList.remove("opened");
+  questionMark.classList.add("unopened");
 });
